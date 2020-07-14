ba91bb8fa9e3058822e5619e47f1fd33d323044f
kikkoro91 <39702057+kikkoro91@users.noreply.github.com>
2020-07-15 04:37:49 +0900

    Fix ruby install (#5)
    * [fix]install and build ruby
    
    * [fix]package install
    
    add install package for ruby install
    
    * [fix]export path to rbenv
    
    not found command "rbenv" in ruby.sh
    
    * [fix]temporaly set path to .bash_profile
    
    follow vim install
    
    * [fix]path to ruby-build/install.sh
    
    * [fix]reload .bash_profile
    
    * [fix]path to rbenv
    
    Co-authored-by: kikkoro91 <kikkoro91>

--- bin/RedHat/ruby.sh
+++ bin/RedHat/ruby.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 # install ruby
 
-sudo yum install ruby-devel
+# install package for ruby install
+sudo yum install -y openssl-devel readline-devel zlib-devel ruby-devel
 
+# install rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
+# temporaly setting path to rbenv for follow command in this shell
+echo `export PATH="$HOME/.rbenv/bin:$PATH"` >> ~/.bash_profile
+source ~/.bash_profile
+
+# init rbenv
 ~/.rbenv/bin/rbenv init
+
+# install rbenv plugin
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+sudo ~/.rbenv/plugins/ruby-build/install.sh
+
+# install ruby latest version
+latest_ver=$(~/.rbenv/bin/rbenv install -l | grep -v - | tail -1)
+
+~/.rbenv/bin/rbenv install $latest_ver 
+
+~/.rbenv/bin/rbenv global $latest_ver

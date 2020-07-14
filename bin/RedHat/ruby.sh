c0ccc166c7688d8000d848da3d8e65e44d41f331
kikkoro91 <kikkoro91>
2020-07-15 02:13:53 +0900

    [fix]install and build ruby

--- bin/RedHat/ruby.sh
+++ bin/RedHat/ruby.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # install ruby
 
-sudo yum install ruby-devel
-
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
 ~/.rbenv/bin/rbenv init
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+sudo ~/.rbenv/plugins/ruby-build.install.sh
+
+latest_ver=$(rbenv install -l | grep -v - | tail -1)
+
+rbenv install $latest_ver 
+
+rbenv global $latest_ver

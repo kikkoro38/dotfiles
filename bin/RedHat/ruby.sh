b05737d93e8962f866b7e1a0b01f5eda0873ba98
kikkoro91 <kikkoro91>
2020-07-15 02:30:21 +0900

    [fix]export path to rbenv
    not found command "rbenv" in ruby.sh

--- bin/RedHat/ruby.sh
+++ bin/RedHat/ruby.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 # install ruby
 
+# install package for ruby install
 sudo yum install -y openssl-devel readline-devel zlib-devel
 
+# install rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
+# setting path to rbenv for follow command in this shell
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+# init rbenv
 ~/.rbenv/bin/rbenv init
 
+# install rbenv plugin
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 sudo ~/.rbenv/plugins/ruby-build.install.sh
 
+# install ruby latest version
 latest_ver=$(rbenv install -l | grep -v - | tail -1)
 
 rbenv install $latest_ver 

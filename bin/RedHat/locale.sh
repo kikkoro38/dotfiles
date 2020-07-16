2780aaffc5adb08fa46185cd26034e9656c3dbec
kikkoro91 <kikkoro91>
2020-07-17 00:50:59 +0900

    reinstall locale-list

--- bin/RedHat/locale.sh
+++ bin/RedHat/locale.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # install ja lang
-sudo yum install -y ibus-kkc vlgothic-*
+sudo yum install -y ibus-kkc vlgothic-* 
+
+sudo yum reinstall -y glibc-common
 
 sudo localectl set-locale LANG=ja_JP.UTF-8
+sudo localectl set-keymap jp106
+sudo localectl set-keymap jp-OADG109A
 
-sudo source /etc/locale.conf
+sudo . /etc/locale.conf
 
 # set timezone
 

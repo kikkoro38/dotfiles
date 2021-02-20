d2ad0a71ecc9f0e9ca5d9b6ef01d1192c74d158d
kikkoro91 <kikkoro91@gmail.com>
2021-02-21 00:40:27 +0900

    add ruby settings

---
+++ bin/Debian/ruby.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# install ruby
+
+# install package for ruby install
+sudo apt install -y bison build-essential libyaml-dev libreadline6-dev libffi-dev libncurses5-dev libgdbm5 libgdbm-dev 
+
+# install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+
+# temporaly setting path to rbenv for follow command in this shell
+echo `export PATH="$HOME/.rbenv/bin:$PATH"` >> ~/.bash_profile
+. ~/.bash_profile
+
+# init rbenv
+~/.rbenv/bin/rbenv init
+
+# install rbenv plugin
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+sudo ~/.rbenv/plugins/ruby-build/install.sh
+
+# install ruby latest version
+latest_ver=$(~/.rbenv/bin/rbenv install -l | grep -v - | grep 2. | tail -1)
+
+~/.rbenv/bin/rbenv install $latest_ver 
+
+~/.rbenv/bin/rbenv global $latest_ver

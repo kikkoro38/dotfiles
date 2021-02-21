fe3c3a6edf84963168c30ec4bfae7b73c5d056c6
kikkoro91 <kikkoro91@gmail.com>
2021-02-22 00:17:22 +0900

    install packages for application before app install

--- bin/Debian/apt.sh
+++ bin/Debian/apt.sh
@@ -3,4 +3,25 @@
  sudo apt update -y
 
 # install utils
-#sudo apt install -y yum-utils gcc lua-devel ncurses-devel libevent-devel
+sudo apt-get install -y \
+  libcurl4-gnutls-dev \
+  libexpat1-dev \
+  gettext \
+  libz-dev \
+  libssl-dev \
+  autoconf \
+  asciidoc \
+  docbook2x \
+  bison \
+  build-essential \
+  libyaml-dev \
+  libreadline6-dev \
+  libffi-dev \
+  libncurses5-dev \
+  libgdbm6 \
+  libgdbm-dev \
+  software-properties-common \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent

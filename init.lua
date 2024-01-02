df622f4e3974db6710dec9d064d3f2e349c09d06
38kozoboy <38kozoboy@USER-KV0ON1A7L2-wsl.localdomain>
2024-01-02 17:05:19 +0900

    必要なライブラリをとりあえず追加

--- init.lua
+++ init.lua
@@ -23,9 +23,9 @@ vim.wo.list = true
 vim.o.listchars = 'eol:↲,tab:▸-,trail:_'
 
 -- tab
-vim.bo.expandtab = true
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 
 -- search
 vim.o.ignorecase = true
@@ -38,6 +38,8 @@ vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
 vim.keymap.set('n', '<S-h>', '^', { noremap = true })
 vim.keymap.set('n', '<S-l>', '$', { noremap = true })
+
+vim.keymap.set('n', '<Esc><Esc>', ':nohlsearch<CR><Esc>')
 
 vim.keymap.set('v', '<C-p>', '"0p', { noremap = true, silent = true })
 
@@ -53,3 +55,100 @@ vim.keymap.set('n', '[TABCMD]c', ':<c-u>tabclose<cr>', { noremap = true, silent 
 vim.keymap.set('n', '[TABCMD]o', ':<c-u>tabonly<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '[TABCMD]s', ':<c-u>tabs<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '[TABCMD]f', ':<c-u>tabfirst<cr>', { noremap = true })
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(
+  {
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd([[colorscheme tokyonight]])
+      end
+    },
+    {
+      "tpope/vim-fugitive"
+    },
+    {
+      "airblade/vim-gitgutter"
+    },
+    {
+      "lambdalisue/fern.vim",
+      keys = { 
+        { "<C-e>", ":Fern . -drawer -toggle -reveal=% <CR>", silent = true }
+      },
+      dependencies = {
+        {
+          "lambdalisue/nerdfont.vim"
+        },
+        {
+          "lambdalisue/fern-renderer-nerdfont.vim",
+          config = function()
+            vim.g['fern#renderer'] = 'nerdfont'
+          end
+        },
+        {
+          'lambdalisue/fern-git-status.vim'
+        }
+      },
+    },
+    {
+      'vim-airline/vim-airline'
+    },
+    {
+      'vim-airline/vim-airline-themes'
+    },
+    {
+      'lambdalisue/battery.vim'
+    },
+    {
+      'tpope/vim-rails'
+    },
+    {
+      'tpope/vim-endwise'
+    },
+    {
+      'ngmy/vim-rubocop'
+    },
+    {
+      'dense-analysis/ale',
+      config = function()
+        vim.g['ale_linters'] = {
+        ruby = { 'rubocop' },
+        javascript = { 'eslint', 'prettier' },
+        typescript = { 'tslint', 'tsserver' },
+        html = {'prettier' }
+        }
+      end
+    },
+    {
+      'prabirshrestha/vim-lsp'
+    },
+    {
+      'mattn/vim-lsp-settings'
+    },
+    {
+      'vim-denops/denops.vim'
+    },
+    {
+      'vim-skk/skkeleton'
+    },
+    {
+      'nvim-treesitter/nvim-treesitter'
+    },
+  }
+)
